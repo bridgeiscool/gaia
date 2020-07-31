3bae3666b49c488240032c9c747e8044fbc8d689
michaelgill <michaelgill@michaels-mbp.fios-router.home>
2020-07-30 23:28:25 -0400

    Add remaining adv tech tiles

--- game/src/main/java/gaia/project/game/model/AdvancedTechTile.java
+++ game/src/main/java/gaia/project/game/model/AdvancedTechTile.java
@@ -16,43 +16,79 @@ public enum AdvancedTechTile {
   Q_C_ACTION {
     @Override
     public String display() {
-      return "ACT: +q +5c";
+      return "ACT: +q,5c";
     }
   },
   SECTOR_SCORING {
     @Override
     public String display() {
-      return "+2VP/SECTOR";
+      return "+2/SECT";
+    }
+  },
+  SECTOR_ORE {
+    @Override
+    public String display() {
+      return "+2o/SECT";
     }
   },
   TP_SCORING {
     @Override
     public String display() {
-      return "+4VP/TP";
+      return "+4/TP";
     }
   },
   M_SCORING {
     @Override
     public String display() {
-      return "+2VP/M";
+      return "+2/M";
+    }
+  },
+  GP_SCORING {
+    @Override
+    public String display() {
+      return "+2/GP";
+    }
+  },
+  FED_SCORING {
+    @Override
+    public String display() {
+      return "+5/FED";
     }
   },
   FED_EOT_SCORING {
     @Override
     public String display() {
-      return "[]: +3VP/FED";
+      return "[]: +3/FED";
+    }
+  },
+  RL_EOT_SCORING {
+    @Override
+    public String display() {
+      return "[]: +3/RL";
+    }
+  },
+  PLANETS_EOT_SCORING {
+    @Override
+    public String display() {
+      return "[]: +1/PT";
     }
   },
   BUILD_MINE {
     @Override
     public String display() {
-      return "+M -> 3VP";
+      return "+M -> 3";
+    }
+  },
+  BUILD_TP {
+    @Override
+    public String display() {
+      return "+TP -> 3";
     }
   },
   TECH_STEP {
     @Override
     public String display() {
-      return "+TECH -> 2VP";
+      return "+TECH -> 2";
     }
   };
 

26d2aba0a2634556ebf1039dc7533b8e4270ea7b
michaelgill <michaelgill@michaels-mbp.fios-router.home>
2020-08-24 15:08:17 -0400

    Implement Hadsch Hallas PI ability

--- game/src/main/java/gaia/project/game/ConversionDialog.java
+++ game/src/main/java/gaia/project/game/ConversionDialog.java
@@ -3,17 +3,24 @@ package gaia.project.game;
 import java.io.IOException;
 
 import gaia.project.game.model.Player;
+import gaia.project.game.model.Race;
 import gaia.project.game.model.Util;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class ConversionDialog extends Dialog<Void> {
   private static final ButtonType DONE = new ButtonType("Done", ButtonData.OK_DONE);
   private final Player activePlayer;
+
+  @FXML
+  private GridPane gridPane;
 
   ConversionDialog(Player activePlayer) {
     this.activePlayer = activePlayer;
@@ -28,6 +35,42 @@ public class ConversionDialog extends Dialog<Void> {
 
     setResultConverter(button -> null);
     getDialogPane().getButtonTypes().add(DONE);
+
+    if (activePlayer.getRace() == Race.HADSCH_HALLAS && !activePlayer.getPi().isEmpty()) {
+      addHHContent();
+    }
+  }
+
+  private void addHHContent() {
+    // Credits -> Qic
+    gridPane.add(new Label("4c"), 0, 11);
+    Button creditsToQic = new Button(">");
+    creditsToQic.setOnAction(e -> {
+      Util.minus(activePlayer.getCredits(), 4);
+      Util.plus(activePlayer.getQic(), 1);
+    });
+    gridPane.add(creditsToQic, 1, 11);
+    gridPane.add(new Label("q"), 2, 11);
+
+    // Credits -> K
+    gridPane.add(new Label("4c"), 0, 12);
+    Button creditsToK = new Button(">");
+    creditsToK.setOnAction(e -> {
+      Util.minus(activePlayer.getCredits(), 4);
+      Util.plus(activePlayer.getResearch(), 1);
+    });
+    gridPane.add(creditsToK, 1, 12);
+    gridPane.add(new Label("k"), 2, 12);
+
+    // Credits -> Ore
+    gridPane.add(new Label("3c"), 0, 13);
+    Button creditsToOre = new Button(">");
+    creditsToOre.setOnAction(e -> {
+      Util.minus(activePlayer.getCredits(), 3);
+      Util.plus(activePlayer.getResearch(), 1);
+    });
+    gridPane.add(creditsToOre, 1, 13);
+    gridPane.add(new Label("o"), 2, 13);
   }
 
   @FXML

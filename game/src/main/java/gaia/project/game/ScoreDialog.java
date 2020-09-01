0e8e2b4fcbfcedcb159216a0f2a7a1b163ba9016
michaelgill <michaelgill@localhost>
2020-09-01 15:18:38 -0400

    Sorts scores highest to lowest, UI tweaks to score dialog

--- game/src/main/java/gaia/project/game/ScoreDialog.java
+++ game/src/main/java/gaia/project/game/ScoreDialog.java
@@ -1,12 +1,16 @@
 package gaia.project.game;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeSet;
 
 import gaia.project.game.model.Game;
 import gaia.project.game.model.Player;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar.ButtonData;
@@ -14,6 +18,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class ScoreDialog extends Dialog<Void> {
@@ -43,9 +48,16 @@ public class ScoreDialog extends Dialog<Void> {
   private void displayScores(Game game) {
     int colIdx = 0;
     for (Player player : game.getPlayers().values()) {
-      top.add(new Label(player.getRace().getRaceName()), colIdx, 0, 2, 1);
+      HBox topBox = new HBox(0);
+      topBox.setAlignment(Pos.CENTER);
+      topBox.getChildren().add(new Label(player.getRace().getRaceName()));
+      top.add(topBox, colIdx, 0, 2, 1);
       int rowIdx = 1;
-      for (Entry<String, Integer> scoreEntry : player.getScoreLog().entrySet()) {
+
+      Set<Entry<String, Integer>> sortedEntries = new TreeSet<>(new HighestVPFirst());
+      sortedEntries.addAll(player.getScoreLog().entrySet());
+
+      for (Entry<String, Integer> scoreEntry : sortedEntries) {
         if (scoreEntry.getValue() != 0) {
           top.add(new Label(scoreEntry.getKey()), colIdx, rowIdx);
           top.add(new Label(format(scoreEntry.getValue())), colIdx + 1, rowIdx);
@@ -77,5 +89,16 @@ public class ScoreDialog extends Dialog<Void> {
         setStyle("-fx-font-weight: bold; -fx-font-size: 16pt;");
       }
     }
+  }
+
+  private static class HighestVPFirst implements Comparator<Entry<String, Integer>> {
+
+    @Override
+    public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
+      return o1.getValue().equals(o2.getValue())
+          ? o1.getKey().compareTo(o2.getKey())
+          : o2.getValue().compareTo(o1.getValue());
+    }
+
   }
 }

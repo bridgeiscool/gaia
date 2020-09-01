752d8bf71eff97f16396c095a253b6c4bc15c9d6
michaelgill <michaelgill@localhost>
2020-08-31 22:58:20 -0400

    Fix Bescods calculation of extra power for federations

--- game/src/main/java/gaia/project/game/model/BescodsPlayer.java
+++ game/src/main/java/gaia/project/game/model/BescodsPlayer.java
@@ -1,5 +1,7 @@
 package gaia.project.game.model;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.TreeSet;
 
 import com.google.common.collect.ImmutableSet;
@@ -7,15 +9,32 @@ import com.google.common.collect.Sets;
 
 import gaia.project.game.PlanetType;
 import gaia.project.game.board.Hex;
+import gaia.project.game.board.HexWithPlanet;
 import javafx.beans.property.SimpleBooleanProperty;
 
 public class BescodsPlayer extends Player {
   private static final long serialVersionUID = 2663152509950065941L;
 
+  private final Set<Coords> grayPlanets = new HashSet<>();
+
   public BescodsPlayer(PlayerEnum playerEnum) {
     super(Race.BESCODS, playerEnum);
 
     getSpecialActions().put(PlayerBoardAction.BUMP_LOWEST_TECH, new SimpleBooleanProperty(false));
+  }
+
+  @Override
+  public void buildMine(HexWithPlanet hex) {
+    super.buildMine(hex);
+    if (hex.getPlanet().getPlanetType() == PlanetType.GRAY) {
+      grayPlanets.add(hex.getCoords());
+    }
+  }
+
+  @Override
+  public void buildSetupMine(HexWithPlanet hex) {
+    super.buildSetupMine(hex);
+    grayPlanets.add(hex.getCoords());
   }
 
   @Override
@@ -39,6 +58,39 @@ public class BescodsPlayer extends Player {
     }
 
     throw new IllegalStateException("No power!");
+  }
+
+  @Override
+  protected int getExcessBuildingPower() {
+    int totalPower = 0;
+
+    for (Coords coords : Sets.union(getMines(), getLostPlanet())) {
+      if (!inFederation(coords)) {
+        totalPower += extraPower(coords) ? 2 : 1;
+      }
+    }
+
+    for (Coords coords : Sets.union(getTradingPosts(), getResearchLabs())) {
+      if (!inFederation(coords)) {
+        totalPower += extraPower(coords) ? 3 : 2;
+      }
+    }
+
+    for (Coords coords : Sets.union(Sets.union(getPi(), getQa()), getKa())) {
+      if (!inFederation(coords)) {
+        totalPower += extraPower(coords) ? getBigBuildingPower().intValue() + 1 : getBigBuildingPower().intValue();
+      }
+    }
+
+    return totalPower;
+  }
+
+  private boolean extraPower(Coords coords) {
+    if (grayPlanets.contains(coords) && !getPi().isEmpty()) {
+      return true;
+    }
+
+    return false;
   }
 
   public int lowestTechLevel() {
